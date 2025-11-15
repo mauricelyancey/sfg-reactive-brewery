6da619f0b42caa605b5c8e304ecdc3167bb14df5
Maurice Yancey <mauricelyancey@gmail.com>
2025-11-15 18:18:23 -0500

    Updated to include more tests and also changed the response of the Get methods to be Reactive types

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -4,15 +4,19 @@ import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
 import guru.springframework.sfgrestbrewery.domain.Beer;
 import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
 import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -29,21 +33,39 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
-    BeerDto validBeer;
+    BeerDto validBeer1,validBeer2, testListBeer1, testListBeer2;
 
     @BeforeEach
     void setUp() {
-        validBeer = BeerDto.builder()
+        validBeer1 = BeerDto.builder()
                 .beerName("Test beer")
                 .beerStyle("PALE_ALE")
                 .upc(BeerLoader.BEER_1_UPC)
                 .build();
+        validBeer2 = BeerDto.builder()
+                .beerName("Test beer")
+                .beerStyle("PALE_ALE")
+                .upc(BeerLoader.BEER_1_UPC)
+                .build();
+
+        testListBeer1 = BeerDto.builder()
+                .beerName("List beer1")
+                .beerStyle("PALE_ALE")
+                .upc(BeerLoader.BEER_1_UPC)
+                .build();
+
+        testListBeer2 = BeerDto.builder()
+                .beerName("List beer2")
+                .beerStyle("PALE_ALE")
+                .upc(BeerLoader.BEER_2_UPC)
+                .build();
+
     }
 
     @Test
     void getBeerById() {
         UUID beerId = UUID.randomUUID();
-        given(beerService.getById(any(), any())).willReturn(validBeer);
+        given(beerService.getById(any(), any())).willReturn(validBeer1);
 
         webTestClient.get()
                 .uri("/api/v1/beer/" + beerId)
@@ -51,6 +73,36 @@ class BeerControllerTest {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Beer.class)
-                .value(beer -> beer.getBeerName(), equalTo(validBeer.getBeerName()));
+                .value(Beer::getBeerName, equalTo(validBeer1.getBeerName()));
+    }
+
+    @Test
+    void listBeers() {
+        List<BeerDto> beerList = Arrays.asList(testListBeer1, testListBeer2);
+
+        BeerPagedList beerPagedList = new BeerPagedList(beerList, PageRequest.of(1,1), beerList.size());
+
+        given(beerService.listBeers(any(),any(),any(),any())).willReturn(beerPagedList);
+
+        webTestClient.get()
+                .uri("/api/v1/beer/")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerPagedList.class);
+    }
+
+    @Test
+    void getBeerByUpc() {
+        String testUpc = BeerLoader.BEER_2_UPC;
+        given(beerService.getByUpc(any())).willReturn(validBeer2);
+
+        webTestClient.get()
+                .uri("/api/v1/beerUpc/" + testUpc)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Beer.class)
+                .value(Beer::getUpc, equalTo(validBeer2.getUpc()));
     }
 }
